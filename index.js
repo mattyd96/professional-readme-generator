ad3db99d91d10939191ac2eefacb4a548fd43f15
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-02-04 21:30:03 +1030

    added basic questions. will need more license options

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your Project Title? ',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'What is the description of your project? ',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions of your project? ',
+        name: 'install'
+    },
+    {
+        type: 'input',
+        message: 'How do you use your app? Please put in your Usage instructions here. ',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Contribution Instructions for other users? ',
+        name: 'contribution'
+    },
+    {
+        type: 'list',
+        message: 'Test instructions? ',
+        name: 'test',
+        choices: ['MIT', 'APACHE', 'CC0']
+    },
+    {
+        type: 'input',
+        message: 'Test instructions? ',
+        name: 'test'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
